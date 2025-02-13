75701b2b98d88086bfca43dd5abe267d1e3dc007
Stacy Saucedo <stacysauc@gmail.com>
2025-02-13 15:10:50 -0600

    Add files via upload

--- Pointer - Lab 1.cpp
+++ Pointer - Lab 1.cpp
@@ -1,20 +1,59 @@
-// Pointer - Lab 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Author: Stacy Saucedo
+// Date: 2/10/25
+// Lab - Pointers Lab 1
+// Purpose: Introduction demonstration of using pointers
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+    /*
+     Section 1 Activities
+     */
+    cout << "Section 1" << endl;
+    // Step 1: Declare and initialize integer variable
+
+    // Step 2: Declare pointer and initialize it with the address of number
+
+    // Step 3: Print the value of number using the variable and the pointer    
+
+    // Step 4: Modify the value of number to 20 using the pointer
+
+    // Step 5: Print the modified value of number using the variable and the pointer
+    cout << endl;
+    /*
+     Section 2 Activities
+     */
+    cout << "Section 2" << endl;
+    // Step 1: Dynamically allocate memory for an integer
+
+    // Step 2: Assign the value 30 to the allocated memory
+
+    // Step 3: Print the value stored in the dynamically allocated memory
+
+    // Step 4: Deallocate the memory and set the pointer to nullptr    
+
+    // Step 5: Print a message confirming the memory deallocation
+
+    cout << endl;
+
+    /*
+     Section 3 Activities
+     */
+    cout << "Section 3" << endl;
+    // Step 1: Dynamically allocate memory for an array of 5 integers
+
+    // Step 2: Assign values to the array elements    
+
+    // Step 3: Print the values of the array elements using the pointer
+    cout << "Array values: ";
+
+    cout << endl;
+    // Step 4: Deallocate the memory and set the pointer to nullptr    
+
+    // Step 5: Print a message confirming the memory deallocation
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
